f36fc3b230054d18c7a52b3a836ad4dd67824a15
María Jesús Senosiain Caamiña <mjsenosiainc@gmail.com>
2015-04-17 21:46:34 +0200

    Project with libraries

---
+++ QRScanner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QRScanner
+//
+//  Created by María Jesús Senosiain Caamiña on 17/04/15.
+//  Copyright (c) 2015 María Jesús Senosiain Caamiña. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
